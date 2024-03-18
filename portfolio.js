8ded4ab2b24cbf8ded6a2cd806186dc872e45c61
vijay <vijaygurani@gmail.com>
2024-03-18 10:16:19 -0700

    My_Portfolio

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+var typed=new Typed(".text",{
+    strings:["Vijay Gurani..."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+}); 
+var typed=new Typed(".mobile",{
+    strings:["8088907850"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+}); 
+
+var typed=new Typed(".email11",{
+    strings:["vijaygurani@gmail.com"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+}); 
